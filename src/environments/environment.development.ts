21492e2f054be07dfe8b0eb48c76a6ef790c43bb
Dinu Petre <92148471+Profudeinfo33@users.noreply.github.com>
2025-12-01 02:46:25 +0200

    sssss

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
   apiUrl: 'https://v3.football.api-sports.io',
-  apiKey: 'fe0aad95e7msh5fa66ed51e011d7p1f6fdcjsn9324e72df7d9'};
+  apiKey: 'da9e885b91359d180d732a75870ce724'};
